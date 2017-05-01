bed699ad6238eb882936a30982cdb0bf0d2e717c
Nicklas Boserup <nickeopti@gmail.com>
2017-05-01 21:09:46 +0200

    Finally a working implementation + some code cleanup

--- src/statistics/TimeAndDateFilterList.java
+++ src/statistics/TimeAndDateFilterList.java
@@ -40,31 +40,40 @@ public class TimeAndDateFilterList {
     public List<DayOfWeek> excludedDays = new ArrayList<>();
     
     public List<Point> getFilteredList(List<Point> list) {
-        List<Point> filteredList = new ArrayList<>(list);
-        Point p = null;
-        outer: for(Iterator<Point> iterator = filteredList.iterator(); iterator.hasNext(); p = iterator.next()) {
+        List<Point> filteredList = new ArrayList<>();
+        for(Point p : list)
+            filteredList.add(new Point(p.x, p.y));
+        
+        Point p;
+        outer: for(int i = 0; i < filteredList.size(); i++) {
+            p = filteredList.get(i);
             if(p == null) continue;
-            LocalDateTime pDate = LocalDateTime.ofEpochSecond((long)p.x, 0, ZoneOffset.UTC); //Works for seconds up to 2^53 - which is way longer than necessary! (ca. year 30.000)
+            LocalDateTime pDate = LocalDateTime.ofEpochSecond(p.x, 0, ZoneOffset.UTC);
             
             if(pDate.toLocalDate().isBefore(dateInterval.startDate) || pDate.toLocalDate().isAfter(dateInterval.endDate)) {
-                iterator.remove();
+                filteredList.remove(i);
+                --i;
+                System.out.println("Remove point based on date interval. Epoc " + pDate.toEpochSecond(ZoneOffset.UTC));
                 continue;
             }
             
             if(pDate.toLocalTime().isBefore(timeInterval.startTime) || pDate.toLocalTime().isAfter(timeInterval.endTime)) {
-                iterator.remove();
+                filteredList.remove(i);
+                --i;
                 continue;
             }
             
             for(DateRange dr : excludedDates) {
                 if(!(pDate.toLocalDate().isBefore(dr.startDate) || pDate.toLocalDate().isAfter(dr.endDate))) {
-                    iterator.remove();
+                    filteredList.remove(i);
+                    --i;
                     continue outer;
                 }
             }
             for(DayOfWeek dw : excludedDays) {
                 if(pDate.getDayOfWeek() == dw) {
-                    iterator.remove();
+                    filteredList.remove(i);
+                    --i;
                     continue outer;
                 }
             }
