1a136e09ee20eb7222bc1e8adcb8d0663b7c82de
Nicklas Boserup <nickeopti@gmail.com>
2017-05-01 20:00:11 +0200

    Initial Commit

---
+++ src/statistics/LongPoint.java
@@ -0,0 +1,12 @@
+package statistics;
+
+/**
+ * @author Nicklas Boserup
+ */
+public class LongPoint extends Point {
+    long x;
+    public LongPoint(long x, double y) {
+        super(x, y);
+        this.x = x;
+    }
+}
